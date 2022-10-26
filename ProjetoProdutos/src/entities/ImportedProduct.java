70afb2c9c1682c7b4df12921058375d6e9374992
Lais Vilela <laisgvilela@gmail.com>
2022-10-26 11:46:51 -0300

    Atualizando ImportedProduct

--- ProjetoProdutos/src/entities/ImportedProduct.java
+++ ProjetoProdutos/src/entities/ImportedProduct.java
@@ -27,7 +27,7 @@ public class ImportedProduct extends Product {
 
 	@Override
 	public String priceTag() {
-		return getName() + " $" + String.format("%.2f", getPrice()) + " (Customs Fee: $"
+		return getName() + " $" + String.format("%.2f", totalPrice()) + " (Customs Fee: $"
 				+ String.format("%.2f", customsFee) + ") ";
 
 	}
